8b25f3b640f8c41ec02833ea08dd5ec13f9215fa
sonu2819 <bsonu2819@gmail.com>
2026-07-05 13:11:57 +0530

    fix problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
